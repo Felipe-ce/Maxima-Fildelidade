1cbb86ccb4178112e556e4c4cb68c910f8e8627b
Felipe Xavier <felipe_rsr@live.com>
2020-10-30 16:45:46 -0300

    Uptade formulario e animações
    Formulario configurado, falta ver as animações e porque o container está estourando.

--- js/script.js
+++ js/script.js
@@ -13,16 +13,16 @@
 
 // }
 
-// if(window.SimpleAnime) {
-// 	new SimpleAnime();
-// }
+if(window.SimpleAnime) {
+	new SimpleAnime();
+}
 
 
 if(window.SimpleForm) {
 	new SimpleForm({
 	  form: ".formphp", // seletor do formulário
 	  button: "#enviar", // seletor do botão
-	  erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@maximafidelidade.com.</p></div>", // mensagem de erro
-	  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve entraremos em contato com você.</p></div>", // mensagem de sucesso
+	  erro: "<div id='form-erro'><h4>Erro no envio!</h4><p>Um erro ocorreu, tente para o email contato@maximafidelidade.com.</p></div>", // mensagem de erro
+	  sucesso: "<div id='form-sucesso'><h4>Formulário enviado com sucesso</h4><p>Em breve entraremos em contato com você.</p></div>", // mensagem de sucesso
 	});
 }
